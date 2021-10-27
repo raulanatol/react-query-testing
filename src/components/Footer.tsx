d069370eadde1161717918a940bdbbefc38a1dba
raulanatol <raul@natol.es>
2021-10-27 19:02:48 +0100

    Invalidation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from 'react';
-import { useMutation, useQuery } from 'react-query';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { addTodoRequest, getTodosRequest } from '../api/todos.api';
 
 export const Footer: FC = () => {
-  const mutation = useMutation('todos', addTodoRequest);
+  const queryClient = useQueryClient();
+  const mutation = useMutation(addTodoRequest, {
+    onSuccess: () => {
+      return queryClient.invalidateQueries('todos');
+    }
+  });
   const { isLoading, data: todos } = useQuery<any>('todos', getTodosRequest);
 
   console.log('Footer', isLoading);
