3272d8064a666460f881ebbaf1839b29fb841c3c
raulanatol <raul@natol.es>
2021-12-09 07:56:03 +0000

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools'
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { TodosPage } from './pages/Todos.page';
 
